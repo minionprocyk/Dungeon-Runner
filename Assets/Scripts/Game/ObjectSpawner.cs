aec9f826e0558b9849a54f8b9d482ddd13a83e55
wprocyk <minion.procyk@gmail.com>
2018-07-15 16:46:55 -0400

    updated assets and inventory

---
+++ Assets/Scripts/Game/ObjectSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner: MonoBehaviour {
+    public List<GameObject> spawnPoints;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	    foreach(GameObject point in spawnPoints)
+        {
+            Instantiate(prefab, point.transform);
+        }	
+	}
+	
+}
