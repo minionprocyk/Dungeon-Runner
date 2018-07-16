30c62de1d154c504d7ea9b918cf0e8cd95318fe3
wprocyk <minion.procyk@gmail.com>
2018-07-15 21:53:36 -0400

    implemented dungeon via triggerable zone

---
+++ Assets/Scripts/Game/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DungeonLoader : MonoBehaviour {
+    public Dungeon dungeon;
+	
+	public void LoadDungeon()
+    {
+        SceneManager.LoadScene(dungeon.SceneName);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Loading new Dungeon: " + dungeon.SceneName);
+        LoadDungeon();
+    }
+}
