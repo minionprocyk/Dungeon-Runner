102ce6a5563d223ffb87ade1aa16ef72b9b9f4c7
wizardseven <joshuadeangoodsell@gmail.com>
2018-07-22 22:08:22 -0400

    Further corrections to leveling system

--- Assets/Scripts/Game/LevelSystem.cs
+++ Assets/Scripts/Game/LevelSystem.cs
@@ -3,18 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelSystem : MonoBehaviour {
-    /*
-    void Start()
-    {
-        //TODO: set these to a normal assigned value. HARD CODE THIS SHIT FOR NOW
-        Player.BaseHealth.Value = 100;
-        Player.BaseEnergy.Value = 100;
-        Level.Value = 0;
-        Experience.Value = 0;
-        ExpToNextLevel.Value = 1;
-        Health.Value = BaseHealth.Value;
-        Energy.Value = BaseEnergy.Value;
-    }
+
+    public Player Player;
 
     void Update()
     {
@@ -22,30 +12,30 @@ public class LevelSystem : MonoBehaviour {
         //TODO: add modifiers, regen, etc
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Experience.Value += 1;
+            Player.Experience.RuntimeValue += 1;
             print("Rewarding exp");
         }
     }
 
     void LevelUp()
     {
-        Level.Value += 1;
-        Experience.Value = Experience.Value - ExpToNextLevel.Value;
-        ExpToNextLevel.Value = Level.Value;// 100 * (Level.Value + 1) * (Level.Value + 1);
+        Player.Level.RuntimeValue += 1;
+        Player.Experience.RuntimeValue = Player.Experience.RuntimeValue - Player.ExpToNextLevel.RuntimeValue;
+        Player.ExpToNextLevel.RuntimeValue = Player.Level.RuntimeValue;// 100 * (Level.Value + 1) * (Level.Value + 1);
 
         //TODO: rewards
-        switch (Level.Value)
+        switch (Player.Level.RuntimeValue)
         {
             case 1:
-                BaseHealth.Value += 25;
+                Player.Health.RuntimeValue += 25;
                 print("Congratulations! Level 1");
                 break;
             case 2:
-                BaseEnergy.Value += 5;
+                Player.Energy.RuntimeValue += 5;
                 print("Congratulations! Level 2");
                 break;
             case 3:
-                BaseHealth.Value += 9875;
+                Player.Health.RuntimeValue += 9875;
                 print("Congratulations! Level 3. You are now much stronger");
                 break;
 
@@ -54,10 +44,10 @@ public class LevelSystem : MonoBehaviour {
 
     void Exp()
     {
-        if (Experience.Value > ExpToNextLevel.Value)
+        if (Player.Experience.RuntimeValue >= Player.ExpToNextLevel.RuntimeValue)
         {
             LevelUp();
         }
     }
-    */
+    
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -9,69 +9,11 @@ public class Player : MonoBehaviour {
     public IntegerVariable Health;              //Health is BaseHealth with modifiers applied
     public IntegerVariable Energy;              //Health is BaseEnergy with modifiers applied
     public PlayerInventory PlayerInventory;
-    public List<Ability> Abilities;
     public IntegerVariable Level;
     public LongVariable Experience;
     public LongVariable ExpToNextLevel;
+    public List<Ability> Abilities;
+    
 
     
 }
-
-
-/* void Start()
-    {
-        //TODO: set these to a normal assigned value. HARD CODE THIS SHIT FOR NOW
-        BaseHealth.Value = 100;
-        BaseEnergy.Value = 100;
-        Level.Value = 0;
-        Experience.Value = 0;
-        ExpToNextLevel.Value = 1;
-        Health.Value = BaseHealth.Value;
-        Energy.Value = BaseEnergy.Value;
-    }
-
-    void Update()
-    {
-        Exp();
-        //TODO: add modifiers, regen, etc
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Experience.Value += 1;
-            print("Rewarding exp");
-        }
-    }
-
-    void LevelUp()
-    {
-        Level.Value += 1;
-        Experience.Value = Experience.Value - ExpToNextLevel.Value;
-        ExpToNextLevel.Value = Level.Value;// 100 * (Level.Value + 1) * (Level.Value + 1);
-
-        //TODO: rewards
-        switch (Level.Value)
-        {
-            case 1:
-                BaseHealth.Value += 25;
-                print("Congratulations! Level 1");
-                break;
-            case 2:
-                BaseEnergy.Value += 5;
-                print("Congratulations! Level 2");
-                break;
-            case 3:
-                BaseHealth.Value += 9875;
-                print("Congratulations! Level 3. You are now much stronger");
-                break;
-            
-        }
-    }
-
-    void Exp()
-    {
-        if (Experience.Value > ExpToNextLevel.Value)
-        {
-            LevelUp();
-        }
-    }
-
-    */
