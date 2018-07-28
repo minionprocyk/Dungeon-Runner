5f37270c67fa8d358c7433eb7fe9e79f5b9b567d
wprocyk <minion.procyk@gmail.com>
2018-07-28 00:28:16 -0400

    added triggerability script, added line renderer to player, updated player to handle test key for ability, added fireball effect

--- Assets/Scripts/ScriptableObjects/RaycastAbility.cs
+++ Assets/Scripts/ScriptableObjects/RaycastAbility.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName ="Character/Ability/RaycastAbility")]
 public class RaycastAbility : Ability
 {
+    public int Damage;
+    public float Range;
+    public float Force;
+
+    public Color Color;
+    private RaycastShootTriggerable rcShoot;
+
     public override void Initialize(GameObject obj)
     {
-        throw new System.NotImplementedException();
+        rcShoot = obj.GetComponent<RaycastShootTriggerable>();
+        rcShoot.Initialize();
+
+        rcShoot.gunDamage = Damage;
+        rcShoot.weaponRange = Range;
+        rcShoot.hitForce = Force;
+        rcShoot.laserLine.material = new Material(Shader.Find("Unlit/Color"));
+        rcShoot.laserLine.material.color = Color;
     }
 
     public override void Trigger()
     {
-        throw new System.NotImplementedException();
+        rcShoot.Fire();
     }
 }
---
+++ Assets/TriggerAbility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class TriggerAbility : MonoBehaviour {
+    public Ability ability;
+    private AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ability.Initialize(this.gameObject);
+            ability.Trigger();
+            audioSource.clip = ability.AbilitySound;
+            audioSource.Play();
+            Instantiate(ability.AbilityEffect, GameObject.Find("ThirdPersonController").transform.position,Quaternion.identity);
+        }
+	}
+}
