6c58ad67eec19766db8487cdf9a4ac1cfc129107
wizardseven <joshuadeangoodsell@gmail.com>
2018-07-29 20:08:40 -0400

    Camera updates. Needs overhaul to third person character

--- Assets/Scripts/Game/CameraScript.cs
+++ Assets/Scripts/Game/CameraScript.cs
@@ -1,33 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class CameraScript : MonoBehaviour
 {
+    
+    private Transform target;
 
-    public float speedH = 10.0f;
-    public float speedV = 0.3f;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    public float cameraSpeedH = 10.0f;
+    public float cameraSpeedV = 0.3f;
 
+    private Vector3 desiredPosition;
+    private Vector3 smoothedPostion;
     private float yaw = 0.0f;
     private float pitch = 0.0f;
-
-
 
     // Use this for initialization
     void Start()
     {
-
+        target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        /*
+        desiredPosition = target.position + offset;
+        smoothedPostion = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPostion;
 
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        transform.LookAt(target);
+        */
+        
+        yaw += cameraSpeedH * Input.GetAxis("Mouse X");
+        pitch -= cameraSpeedV * Input.GetAxis("Mouse Y");
         if(pitch < -90.0 ) { pitch = -90.0f; }
         else if(pitch > 90.0) { pitch = 90.0f; }
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        
 
     }
 }
